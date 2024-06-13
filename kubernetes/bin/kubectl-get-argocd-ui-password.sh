f5a1216812007d60696c06fb700d098bb9c60d30
Hugo O. Rivera (ROGUH) <hugo.rivera@powerflex.com>
2024-06-13 10:27:54 -0600

    improve argocd password fetcher

--- kubernetes/bin/kubectl-get-argocd-ui-password.sh
+++ kubernetes/bin/kubectl-get-argocd-ui-password.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+TYPE="${TYPE:-0}"
 echo Remember to port forward the UI with >&2
 echo kubectl port-forward svc/argocd-server -n argocd 8080:80 >&2
 set -x
-# kubectl get secret argocd-cluster -n argocd -o jsonpath='{.data.admin\.password}' | base64 --decode
+[ $TYPE -eq 1 ] && kubectl get secret argocd-cluster -n argocd -o jsonpath='{.data.admin\.password}' | base64 --decode
 # This is just the password hash, don't want this
 # kubectl get secret argocd-secret -n argocd -o jsonpath='{.data.admin\.password}' | base64 --decode
-kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 --decode
+[ $TYPE -eq 0 ] && kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 --decode
