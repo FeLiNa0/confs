40331416d2e98f64800c613a9395144b19e98ae6
Hugo O. Rivera (ROGUH) <hugo.rivera@powerflex.com>
2024-12-24 09:50:01 -0700

    2025: less config!

--- fish_the_best_sh/.config/fish/config.fish
+++ fish_the_best_sh/.config/fish/config.fish
@@ -7,7 +7,7 @@ set DEBUG_OUTPUT false
 function debug
   if status is-interactive && [ "$DEBUG_OUTPUT" = true ]
     set_color -i
-    echo "$FISH_LOGO $argv"
+    echo "$FISH_LOGO $(date +%s.%N) $argv"
     set_color normal
   end
 end
@@ -45,29 +45,31 @@ function set_global
   debug Set variable $argv[1]
 end
 
-source_if_exists $HOME/.config/fish/local_env.fish
+# source_if_exists $HOME/.config/fish/local_env.fish
 # set_global FISH_LOGO 🐠
 
 # Common binary paths
 addpaths $HOME/bin --verbose
 addpaths $HOME/.local/bin  --verbose
 # Lua
-addpaths $HOME/.luarocks/bin  --verbose
+# addpaths $HOME/.luarocks/bin  --verbose
 # Rust binaries
 addpaths $HOME/.cargo/bin
 # CUDA binaries
-addpaths /opt/cuda/bin
-# ASDF from Arch's AUR
-addpaths /opt/asdf-vm/bin
+# addpaths /opt/cuda/bin
 # Raku (Perl 6)
-addpaths $HOME/.rakudo-moar-2024.01-01-linux-x86_64-gcc/bin/
+# addpaths $HOME/.rakudo-moar-2024.01-01-linux-x86_64-gcc/bin/
 # Snap Linux binaries
-addpaths /snap/bin
+# addpaths /snap/bin
 # Google Gcloud
-addpaths /opt/google-cloud-cli/bin/
+# addpaths /opt/google-cloud-cli/bin/
 
-# ASDF
+# still needed?
+# set_global MANPATH $MANPATH /usr/share/man /usr/local/share/man/
+
+# ASDF language version manager
 source_if_exists /opt/asdf-vm/asdf.fish
+addpaths /opt/asdf-vm/bin  # from AUR
 
 set_global EDITOR vis
 
@@ -76,55 +78,41 @@ set_global EDITOR vis
 set --unexport XDG_GREETER_DATA_DIR
 set_global CUDA_LIB /opt/cuda/targets/x86_64-linux/lib/
 
-# This is used for speeding up integration/unit tests on a private repo
-set_global TEST_TIMEOUT_SCALING_FACTOR 2
-
-set_global DEVICE_MANAGER_FORCE_PASS true
-
+# set_global DEVICE_MANAGER_FORCE_PASS true
 # For kubectl
-set_global USE_GKE_GCLOUD_AUTH_PLUGIN True
+# still needed? set_global USE_GKE_GCLOUD_AUTH_PLUGIN True
 
 # Disable legacy algorithms in OpenSSL 3.0+
 # A fix when using the Python cryptography library.
-set_global CRYPTOGRAPHY_OPENSSL_NO_LEGACY true
+# still needed? set_global CRYPTOGRAPHY_OPENSSL_NO_LEGACY true
 
 # Load some common python libraries
 set_global PYTHONSTARTUP "$HOME/.ipython/profile_default/startup/10-imports.py"
-
-if command -v most > /dev/null 2>&1
-    set_global PAGER most
-    set_global LESS $PAGER
-    set_global MANPAGER $PAGER
-    set_global SYSTEMD_PAGER $PAGER
-    alias less=$PAGER
-end
 
 # Load aliases before abbreviations
 # I tried getting rid of tryalias with "alias tryalias alias", but it's actually
 # almost as fast as plain alias so I'll keep it.
 source_if_exists $HOME/.aliases --verbose
+
 # commacomma is defined as a fish function so should not be shared with other shells
 alias ,,=commacomma
 
-
 abbr l 'ls -F -a'
-abbr ll 'l -l -h'
+abbr ll 'ls -F -a -l -h'
 abbr e evince   # alternatives: 'zathura --fork' mupdf mupdf-x11 qpdfview 'wine READER10.exe'
 abbr feh viewnior
 abbr MONKEY 'echo MONKEY'
 
-
 abbr k "rlwrap ngnk"  # The K language
-abbr mc "cd ~/src/min*"
 
 # Python and Scientific commands
 abbr py python3
 abbr ipy ipython3
-abbr sci "ipython3 -i -c 'import numpy as np, scipy, sympy, astropy; from numba import jit'"
+## abbr sci "ipython3 -i -c 'import numpy as np, scipy, sympy, astropy; from numba import jit'"
 abbr jwt_decode "python3 -c \"import jwt,json ; print(json.dumps(jwt.api_jwt.decode(input('token> '), options={'verify_signature': False}), indent=2)) # Please run pip install PyJWT if this fails\""
 
 # Common directories
-abbr leet "cd ~/src/golf/speed/*/1/3/3/7/*/*/"
+abbr mc "cd ~/src/min*"
 abbr art "cd ~/src/art/"
 abbr games "cd ~/src/games/"
 abbr golf "cd ~/src/golf/"
@@ -147,39 +135,39 @@ end
 # Github-specific shortcuts
 if command -v gh > /dev/null
     abbr ghch 'gh pr checkout'
-    abbr ghw 'gh pr checks --watch'
-    abbr ghwm 'gh pr checks --watch && gh pr merge --delete-branch --merge'
+    ## abbr ghw 'gh pr checks --watch'
+    ## abbr ghwm 'gh pr checks --watch && gh pr merge --delete-branch --merge'
 end
 
 # KUUUUUUUUUUUUUUUBBBBBBBBBBBBBBBBB
 if command -v kubectl > /dev/null
-    # TODO kubectl get apps -A | grep -v Synced 
     abbr k kubectl
     abbr kx kubectx
-    abbr kc 'kubectl config'
-    abbr kcc 'kubectl config current-context'
-    abbr whereami 'kubectl config current-context'
-    abbr kcn 'kubectl config set-context --current --namespace'
+    abbr kcp 'kubectl cp'
     # List and detail resources
     abbr kg 'kubectl get'
     abbr kgp 'kubectl get pods'
-    abbr kgs 'kubectl get services'
-    abbr kgcm 'kubectl get configmaps'
-    abbr kgi 'kubectl get ingress'
     abbr kd 'kubectl describe'
-    abbr ktop 'kubectl top'
     # Debugging pods
-    abbr kl 'kubectl logs'
-    abbr kcp 'kubectl cp'
+    abbr kl stern  ## Kubectl logs
     abbr kex 'kubectl exec'
     abbr kpf 'kubectl port-forward'
     # Emergency/local modifications. Prefer devops. Ensure correct cluster is targeted.
-    abbr kdl 'correct-kubernetes-cluster.sh && kubectl delete'
+    ## abbr kdl 'correct-kubernetes-cluster.sh && kubectl delete'
     abbr krr 'correct-kubernetes-cluster.sh && kubectl rollout restart'
-    abbr kubectl-stop-sync-app 'correct-kubernetes-cluster.sh && kubectl -n argocd patch --type=merge application -p "{\"spec\":{\"syncPolicy\":null}}"'
-    abbr kubectl-start-sync-app 'correct-kubernetes-cluster.sh && kubectl -n argocd patch --type=merge application -p "{\"spec\":{\"syncPolicy\":{\"automated\":{\"selfHeal\":true}}}}"'
+    ## abbr kubectl-stop-sync-app 'correct-kubernetes-cluster.sh && kubectl -n argocd patch --type=merge application -p "{\"spec\":{\"syncPolicy\":null}}"'
+    ## abbr kubectl-start-sync-app 'correct-kubernetes-cluster.sh && kubectl -n argocd patch --type=merge application -p "{\"spec\":{\"syncPolicy\":{\"automated\":{\"selfHeal\":true}}}}"'
     debug Setup Kubernetes abbreviations
-    addpaths $HOME/.krew/bin
+    #### Rarely used:
+    ## kubectl get apps -A | grep -v Synced 
+    ## abbr kc 'kubectl config'
+    ## abbr kcc 'kubectl config current-context'
+    ## abbr whereami 'kubectl config current-context'
+    ## abbr kcn 'kubectl config set-context --current --namespace'
+    ## abbr kgs 'kubectl get services'
+    ## abbr kgcm 'kubectl get configmaps'
+    ## abbr kgi 'kubectl get ingress'
+    ## abbr ktop 'kubectl top'
 end
 
 if command -v aws > /dev/null 2>&1
@@ -189,11 +177,10 @@ if command -v aws > /dev/null 2>&1
 end
 
 if true
-    abbr ff 'cd ~/pf && cd ~/pf/powerflex_edge_traffic_manager'
+    ## abbr ff 'cd ~/pf && cd ~/pf/powerflex_edge_traffic_manager'
     abbr cs 'cd ~/pf && cd ~/pf/powerflex_edge_ocpp_central_system'
     abbr ev 'cd ~/pf && cd ~/pf/pfc_ev'
     abbr devman 'cd ~/pf && cd ~/pf/powerflex_cloud_edge_device_manager'
-    abbr devadm 'cd ~/pf && cd ~/pf/pfc_ev/device-administrator'
     abbr scale 'cd ~/pf && cd ~/pf/small_scale'
     abbr uplo 'cd ~/pf && ~/pf/pfc_site_uploader'
     abbr natsinfra 'cd ~/pf && cd ~/pf/pfc_nats_infrastructure/'
@@ -206,11 +193,13 @@ if command -v docker > /dev/null
     abbr dex 'docker exec'
     abbr dck "docker container kill (docker container ls --format json | jq 'select(.Networks != \"kind\") | .ID' | sed 's/\"//g')"
     abbr docker-norestart "docker update --restart=no (docker container ls --format json | jq 'select(.Networks != \"kind\") | .ID' | sed 's/\"//g')"
-    abbr dck-all 'docker container kill (docker ps -q)'
-    set_global BUILDKIT_PROGRESS plain
-    set_global DOCKER_BUILDKIT 1
+    ## abbr dck-all 'docker container kill (docker ps -q)'
     debug Setup Docker abbreviations
 end
+    
+## still needed? set_global BUILDKIT_PROGRESS plain
+## still set_global DOCKER_BUILDKIT 1
+## still needed? addpaths $HOME/.krew/bin
 
 if command -v makeanywhere > /dev/null
     set -g MAKEANYWHERE (command -v makeanywhere)
@@ -229,13 +218,11 @@ function pmake --wraps make --description "pma --wraps make pipenv run"
     python -m pipenv run make $argv
 end
 
-set_global MANPATH $MANPATH /usr/share/man /usr/local/share/man/
-
-# darwin with some patches
-addpaths /usr/local/opt/gettext/bin
-addpaths /Library/Frameworks/Python.framework/Versions/3.7/bin
-addpaths /usr/local/opt/openssl@1.1/bin
-addpaths /usr/local/opt/openssl/bin
+# adjust PATH for a darwin OS with certain patches (MacOS)
+## addpaths /usr/local/opt/gettext/bin
+## addpaths /Library/Frameworks/Python.framework/Versions/3.7/bin
+## addpaths /usr/local/opt/openssl@1.1/bin
+## addpaths /usr/local/opt/openssl/bin
 
 # Have fzf use ag to find files
 if command -v ag > /dev/null 2>&1
@@ -244,20 +231,6 @@ if command -v ag > /dev/null 2>&1
     set_global FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
     debug fzf will now use the silver searcher ag
   end
-end
-
-function miniconda_fish_init
-  # If this function is overwriting your system's Python:
-  # conda config --set auto_activate_base false
-  # set --local CONDA_BIN "$HOME/miniconda3/bin/conda"
-  set --local CONDA_BIN "/opt/miniconda3/bin/conda"
-  # set --local CONDA_BIN "/usr/bin/conda"
-  if ! command -v "$CONDA_BIN" > /dev/null
-    return 1
-  end
-  eval "$CONDA_BIN" "shell.fish" "hook" | source
-  conda activate $argv[1]
-  debug Loaded miniconda and the $argv[1] environment
 end
 
 if status is-interactive
@@ -278,20 +251,20 @@ if status is-interactive
     starship init fish | source
   end
   
-  set TOTAL_STARTUP_TIME (echo (date +%s.%N) "$START_TIME" | awk '{print ($1 - $2) * 1000}' || echo UNKNOWN)
-  if status is-interactive && [ "$DEBUG_OUTPUT" = true ]
-    echo "$TOTAL_STARTUP_TIME"ms
-  end
-
   set_global fzf_preview_file_cmd cat
 
-  if command -v fzf 2>&1 >/dev/null && ! grep PatrickF1/fzf.fish ~/.config/fish/fish_plugins >/dev/null
+  if ! grep PatrickF1/fzf.fish ~/.config/fish/fish_plugins >/dev/null && command -v fzf 2>&1 >/dev/null
     # Dependencies are:
     # fzf
     # fd, find alternative https://github.com/sharkdp/fd (fd-find in fedora)
     # optional bat, cat replacement
     echo 'Installing fzf.fish https://github.com/PatrickF1/fzf.fish for git log, git status, ctrl-p (file search), and ctrl-r (history)'
     fisher install PatrickF1/fzf.fish
+  end
+  
+  set TOTAL_STARTUP_TIME (echo (date +%s.%N) "$START_TIME" | awk '{print ($1 - $2) * 1000}' || echo UNKNOWN)
+  if status is-interactive ## && [ "$DEBUG_OUTPUT" = true ]
+    echo "$TOTAL_STARTUP_TIME"ms
   end
 
   # RUN LAST so user can ctrl-c
@@ -307,6 +280,7 @@ function install_plugin_manager
 end
 
 function install_plugins
+  ########### Use this function on first run to install plugins
   # Notification when commands finish
   fisher install franciscolourenco/done
   fisher install PatrickF1/fzf.fish
@@ -314,3 +288,19 @@ function install_plugins
   gh completion --shell fish > ~/.config/fish/completions/gh.fish || echo "No gh"
   omnictl completion fish > ~/.config/fish/completions/omnictl.fish || echo "No omnictl"
 end
+
+function miniconda_fish_init
+  ########### Use this function to init miniconda (Anaconda distribution)
+  # If this function is overwriting your system's Python:
+  # conda config --set auto_activate_base false
+  # set --local CONDA_BIN "$HOME/miniconda3/bin/conda"
+  set --local CONDA_BIN "/opt/miniconda3/bin/conda"
+  # set --local CONDA_BIN "/usr/bin/conda"
+  if ! command -v "$CONDA_BIN" > /dev/null
+    return 1
+  end
+  eval "$CONDA_BIN" "shell.fish" "hook" | source
+  conda activate $argv[1]
+  debug Loaded miniconda and the $argv[1] environment
+end
+
--- fish_the_best_sh/.config/fish/functions/fish_greeting.fish
+++ fish_the_best_sh/.config/fish/functions/fish_greeting.fish
@@ -1,13 +1,6 @@
 function fish_greeting
   if [ "$PWD" = "$HOME" ]
     printf "%s %s\n" "Mater artium necessitas"
-    uname -mnors
+    uname -mnor
   end
-  # fish_prompt
-  # echo
-  # if command -v exa > /dev/null 2>&1
-  #   exa "$PWD"
-  # else
-  #   ls "$PWD"
-  # end
 end
