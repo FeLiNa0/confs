a52a9aae56cf7a27bef15b16cf781ecd1eb929f9
Hugo O. Rivera (ROGUH) <hugo.rivera@powerflex.com>
2025-04-28 07:01:49 -0600

    misc updates

--- backup/bin/backup-games.sh
+++ backup/bin/backup-games.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT=$(get-backup-root.sh)/games/$(date +%Y-%m)
+ROOT=$(get-backup-root.sh)/$(date --iso-8601)
 
 mkdir -p $ROOT/game-backups
 
@@ -12,15 +12,10 @@ echo BACKUP WEBFISHING
 # $HOME/.steam/steam/steamapps/compatdata/3146520/pfx/drive_c/users/steamuser/AppData/Roaming/Godot/app_userdata/webfishing_2_newver
 rsync $RSYNC_OPTS \
   $HOME/.steam/steam/steamapps/compatdata/3146520/pfx/drive_c/users/steamuser/AppData/ \
-  $ROOT/game-backups/Webfishing
-
-echo BACKUP FINAL FANTASY
-# $HOME/.steam/steam/steamapps/compatdata/1173770/pfx/drive_c/users/steamuser/Documents/My Games/FINAL FANTASY PR
-rsync $RSYNC_OPTS \
-  "$HOME/.steam/steam/steamapps/compatdata/1173770/pfx/drive_c/users/steamuser/Documents/My Games/FINAL FANTASY PR" \
-  $ROOT/game-backups/FinalFantasy
+  $ROOT/game-backups
 
 echo BACKUP CALL OF DUTY 2 2005
 rsync $RSYNC_OPTS \
-  "$HOME/.local/share/Steam/steamapps/common/Call of Duty 2/main/players" \
-  $ROOT/game-backups/COD2_2025
+  "$HOME/.steam/steam/steamapps/common/Call of Duty\ 2/main/players/" \
+  $ROOT/game-backups
+
