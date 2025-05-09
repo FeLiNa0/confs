1aa678776ebf7790684c4ed6bd29112b24640b3d
ROGUH Rivera Calzadillas <roguh@roguh.com>
2025-05-09 09:13:48 -0600

    welcome back ollama

--- ollama/bin/ollama-setup.sh
+++ ollama/bin/ollama-setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-if ! command -v nvidia-smi >/dev/null 2>&1 ; then
-    echo "WARNING: Are NVidia drivers installed?"
-fi
+### RIP
+###if ! command -v nvidia-smi >/dev/null 2>&1 ; then
+###    echo "WARNING: Are NVidia drivers installed?"
+###fi
 
 if ! command -v ollama >/dev/null 2>&1 ; then
     echo "ERROR: ollama not installed"
 fi
 
-for model in codellama llama2 mistral; do
+for model in codellama llama2 mistral mixtral mistral-small3.1 mistral-large; do
     ollama pull $model
     notify-send "Model $model downloaded"
 done
--- ollama/bin/ollama.sh
+++ ollama/bin/ollama.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
-OUT_BASE="$HOME/ollama-logs"
+# try these models: codellama llama2 mistral mixtral mistral-small3.1 mistral-large
+# see here for more https://ollama.com/library
+
+#OUT_BASE="$HOME/ollama-logs"
+
+# Configuration
+OUT_BASE="$HOME/Dropbox/sync/technical/ollama-logs"
 OUT="$OUT_BASE/ollama-$(date +%Y-%m-%d_%H-%M-%S).txt"
-echo ollama "$@" >> "$OUT"
 set -x
-OLLAMA_NOPRUNE=true ollama "$@" | tee --append "$OUT"
+
+# Record total time
+START_TIME="$(date +%s.%N)"
+
+# Print command
+echo ">>>> ollama $@" >> "$OUT"
+echo >> "$OUT"
+echo >> "$OUT"
+
+# Run ollama
+# Also use grep and sed to clear any control characters or shell escape sequences
+OLLAMA_NOPRUNE=true ollama "$@" 2>&1 | \
+  grep "[[:print:][:space:]]*" | sed -e "s/\x1b\[.\{1,5\}m//g" | tee --append "$OUT"
+
+# Record total time
+TOTAL_TIME="$(echo "$(date +%s.%N) $START_TIME" | awk '{print ($1 - $2)}' || echo UNKNOWN)"
+
+echo >> "$OUT"
+echo >> "$OUT"
+echo "#### Total runtime: $TOTAL_TIME sec" | tee --append "$OUT"
