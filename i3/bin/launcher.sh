20dee39726438ad5d4ed2def8e5a5881738c12e3
ROGUH Rivera Calzadillas <roguh@roguh.com>
2025-12-08 07:57:32 -0700

    haskell, rofi

--- i3/bin/launcher.sh
+++ i3/bin/launcher.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -
+ROFI_ARGS="$2"
+ROFI_MODE="$1"  # Default -combi, try -recursivebrowser
 if command -v rofi > /dev/null
 then
     # -normalize-match -- Normalize characters with accents so plain letter matches them, e -> é
     rofi \
       -me-select-entry '' \
-      -matching fuzzy \
-      -show ${1:-combi} \
+      "${ROFI_ARGS:-matching fuzzy}" \
+      -show "${ROFI_MODE:-combi}" \
       -fixed-num-lines \
       -eh 2 \
       -modi combi,run,window,ssh,keys \
