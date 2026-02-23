a8ff49b8b65e6470403f0b3ea05c7a2d84bca7c7
Felina Rivera Calzadillas (ROGUH) <felina.rivera@powerflex.com>
2026-02-23 12:57:53 -0700

    motto

--- fish_the_best_sh/.config/fish/functions/fish_greeting.fish
+++ fish_the_best_sh/.config/fish/functions/fish_greeting.fish
@@ -1,6 +1,6 @@
 function fish_greeting
   # set MOTTO "Mater artium necessitas"
-  set MOTTO "CRESCIT EUNDO"
+  set MOTTO "Crescit Eundo"
   if [ "$PWD" = "$HOME" ]
     printf "%s %s\n" "$MOTTO $(uname -mnor)"
   end
